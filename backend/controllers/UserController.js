8042b0cf44e10a2f0b30a76360e5d6a016b8cd19
Samuel-Sales <cacabrbf@hotmail.com>
2023-12-01 11:53:37 -0300

    Envio de imagens done

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -43,8 +44,80 @@ const register = async(req, res) => {
         _id: newUser._id,
         token: generateToken(newUser._id),
     });
-}
+};
+
+// Sign user in
+const login = async (req, res) => {
+    const {email, password} = req.body
+    const user = await User.findOne({email})
+
+    // Check if user exists
+    if (!user) {
+        res.status(404).json({errors: ["Usuário não encontrado"]});
+        return;
+    }
+
+    // Check if password matches
+    if (!(await bcrypt.compare(password, user.password))) {
+        res.status(422).json({erros: ["Senha inválida"]})
+        return;
+    }
+
+    // Return user with token
+    res.status(201).json({
+        _id: user._id,
+        profileImage: user.profileImage,
+        token: generateToken(user._id),
+    });
+};
+
+// Get current logged in user
+const getCurrentUser = async (req, res) => {
+    const user = req.user;
+    res.status(200).json(user);
+};
+
+// Update an user
+const update = async (req, res) => {
+    const {name, password, bio} = req.body;
+    let profileImage = null;
+
+    if (req.file) {
+        profileImage = req.file.filename
+    }
+
+    const reqUser = req.user
+
+    const user = await User.findById(new mongoose.Types.ObjectId(reqUser._id)).select('-password');
+
+    if (name) {
+        user.name = name;
+    }
+
+    if (password) {
+        // Generate password hash
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        user.password = passwordHash
+    }
+
+    if (profileImage) {
+        user.profileImage = profileImage;
+    }
+
+    if (bio) {
+        user.bio = bio;
+    }
+
+    await user.save()
+
+    res.status(200).json(user);
+};
 
 module.exports = {
     register,
+    login,
+    getCurrentUser,
+    update,
 }
